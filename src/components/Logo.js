36305d24570a786e4e3718b36d47dfebace422c6
rama11 <prof.agastyo@gmail.com>
2022-11-03 22:21:05 +0700

    'gitignore'

---
+++ src/components/Logo.js
@@ -0,0 +1,32 @@
+import React, { Component } from 'react';
+import {
+  StyleSheet,
+  Text,
+  View,
+   Image 
+} from 'react-native';
+
+export default class Logo extends Component<{}> {
+	render(){
+		return(
+			<View style={styles.container}>
+				<Image  style={{width:70, height: 70}}
+          			source={require('../images/logo.png')}/>
+          		<Text style={styles.logoText}>Ruang Sehat </Text>	
+  			</View>
+			)
+	}
+}
+
+const styles = StyleSheet.create({
+  container : {
+    flexGrow: 1,
+    justifyContent:'flex-end',
+    alignItems: 'center'
+  },
+  logoText : {
+  	marginVertical: 15,
+  	fontSize:18,
+  	color:'rgb(0, 0, 0)'
+  }
+});
